901502274fc8597d38256b7ad5a7ccfd3753e8ec
Muhammad Lutfi <149035619+llltfiii@users.noreply.github.com>
2026-07-01 18:50:01 +0700

    Delete finance-backend directory

--- finance-backend/dist/validators/category.validator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.categorySchema = void 0;
-const zod_1 = require("zod");
-exports.categorySchema = zod_1.z.object({
-    name: zod_1.z.string().min(1, "Category name is required"),
-    type: zod_1.z.enum(["income", "expense"], { message: "Type must be either 'income' or 'expense'" }),
-});
--- finance-backend/src/validators/category.validator.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-export const categorySchema = z.object({
-  name: z.string().min(1, "Category name is required"),
-  type: z.enum(["income", "expense"], { message: "Type must be either 'income' or 'expense'" }),
-});
